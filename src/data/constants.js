a31e398d4b07aa772eb7ae06d119b48d9f493f4e
Nikhil Rao <nrao8658@gmail.com>
2025-02-20 12:47:38 +0530

    Update constants.js(Resume)

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/NikhilRao8658",
   resume:
-    "https://drive.google.com/file/d/1jZPh7BPTpdyjbJb4cbNpFe97-13om-Ip/view?usp=sharing",
+    "https://drive.google.com/file/d/1sOZmfhZ1l-ixi3x5MSahJBdxtOX2YCTT/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/nikhil8658/",
   twitter: "",
   insta: "",
